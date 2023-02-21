3279bf83508aa49497c8ec94c23e67bbb075ad96
CyberMuffin <cybermuffinofficial@gmail.com>
2023-02-21 07:46:54 +0530

    Add project files.

---
+++ MauiProgram.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiBuberBreakfast;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("Roboto-Bold.ttf", "Roboto-Bold");
+                fonts.AddFont("Roboto-Regular.ttf", "Roboto-Regular");
+            });
+
+        return builder.Build();
+    }
+}
