0ef610e01a152ea1bd3ab69d293c8e71bea9edae
CyberMuffin <cybermuffinofficial@gmail.com>
2023-02-21 20:41:45 +0530

    Added refresh view and relaycommand annotation for refreshing

--- ViewModels/MyBreakfastsViewModel.cs
+++ ViewModels/MyBreakfastsViewModel.cs
@@ -8,36 +8,49 @@ public partial class MyBreakfastsViewModel : ObservableObject
     [ObservableProperty]
     List<Breakfast> breakfasts;
 
+    [ObservableProperty]
+    bool isRefreshing;
+
     public MyBreakfastsViewModel()
     {
-        LoadBreakfasts();
+        LoadBreakfastsAsync();
     }
 
     [RelayCommand]
-    public void LoadBreakfasts()
+    public async Task LoadBreakfastsAsync()
     {
-        Breakfasts = new()
+        try
         {
-            new Breakfast
-            (
-                Name : "Vegan Sunshine",
-                Description : "Vegan Sunshine",
-                StartDateTime : DateTime.UtcNow.AddDays(1),
-                EndDateTime : DateTime.UtcNow.AddDays(1).AddHours(1),
-                Image : new Uri("https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"),
-                Savory : new List < string >() { "Toast", "Toast", "Toast", "Toast" },
-                Sweet : new List < string >() { "Cookie", "Cookie", "Cookie", "Cookie" }
-            ),
-            new Breakfast
-            (
-                Name : "Breakfast @ Tiffany's",
-                Description : "Tiffany",
-                StartDateTime : DateTime.UtcNow.AddDays(1),
-                EndDateTime : DateTime.UtcNow.AddDays(1).AddHours(1),
-                Image : new Uri("https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=820&q=80"),
-                Savory : new List < string >() { "Salad" },
-                Sweet : new List < string >() { "Waffle" }
-            )
-        };
+            await Task.Delay(2000);
+            Breakfasts = new()
+            {
+                new Breakfast
+                (
+                    Name : "Vegan Sunshine",
+                    Description : "Vegan Sunshine",
+                    StartDateTime : DateTime.UtcNow.AddDays(1),
+                    EndDateTime : DateTime.UtcNow.AddDays(1).AddHours(1),
+                    Image : new Uri("https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"),
+                    Savory : new List < string >() { "Toast", "Toast", "Toast", "Toast" },
+                    Sweet : new List < string >() { "Cookie", "Cookie", "Cookie", "Cookie" }
+                ),
+                new Breakfast
+                (
+                    Name : "Breakfast @ Tiffany's",
+                    Description : "Tiffany",
+                    StartDateTime : DateTime.UtcNow.AddDays(1),
+                    EndDateTime : DateTime.UtcNow.AddDays(1).AddHours(1),
+                    Image : new Uri("https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=820&q=80"),
+                    Savory : new List < string >() { "Salad" },
+                    Sweet : new List < string >() { "Waffle" }
+                )
+            };
+        }
+
+
+        finally
+        {
+            IsRefreshing = false;
+        }
     }
 }
